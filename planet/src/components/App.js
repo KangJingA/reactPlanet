4237c13d9edb59bc5485f7eceb6a23129a6d04aa
Nicholas Ang <66071629+KangJingA@users.noreply.github.com>
2021-03-18 20:43:15 +0800

    QR reader

--- planet/src/components/App.js
+++ planet/src/components/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import QRreader from './QRreader';
+
 //import Globe from './Globe';
-import Searchbar from './Searchbar';
+// import Searchbar from './Searchbar';
 //import SpeciesNameConvertor from '../api/SpeciesNameConvertor'
+
 const App = () => {
 
     return (
         <div>
-            Hello
-            <Searchbar></Searchbar>
+            <QRreader></QRreader>
         </div>
     )
 }
